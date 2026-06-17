77472b52ccdb5efdfaf3cfe810fb3d4dc15907d9
Amanda <amandancaldas85@gmail.com>
2026-06-17 17:22:31 -0300

    Agora com o Brasão da Instituição.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -57,7 +57,7 @@ const LoginView = () => {
           <div className="text-center mb-8">
           <div className="text-center mb-8">
           <img 
-            src="/assets/brasao-degase-300.png"
+            src="./assets/brasao-degase-300.png"
             alt="Brasão DEGASE" 
             className="mx-auto h-20 w-auto mb-3" 
           />
